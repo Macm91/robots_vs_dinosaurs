ab3929f6a6f304e006724aeff43cd8b0997a5da8
Megan Mandes <meganmandes@Megans-Air.lan>
2021-08-11 12:17:13 -0400

    added the weapon class and called it inthe main.py file as 'sword' 'gun' and 'saw'.

---
+++ main.py
@@ -0,0 +1,11 @@
+from Weapon import weapon
+
+sword = weapon
+sword.weapon_details(sword, "katana", 10)
+
+gun = weapon
+gun.weapon_details(gun, "pistol", 30)
+
+saw = weapon
+saw.weapon_details(saw, "chainsaw", 40)
+
